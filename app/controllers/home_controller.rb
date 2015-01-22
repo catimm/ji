311c670e1d294d52768fc1c2b90121e96f3b1c9d
Carl Timm <ctiv@hotmail.com>
2015-01-21 16:19:32 -0800

    Removed Ziggeo and added Camera Tag

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,7 @@
 class HomeController < ApplicationController
   
   def index  
-    # Ziggeo credentials
-    ziggeo = Ziggeo.new("a43572f97516b35e30e36c1ccff7a9fc", "a69ee2ed945bcc6273a7d09edc59ef92", "0c92b79b4097640d4ead73efaa5bdd23")
+    
   end
   
   def video_choice 
--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -1,7 +1,6 @@
 class InputController < ApplicationController
   def first  
-    # Ziggeo credentials
-    @ziggeo = Ziggeo.new("a43572f97516b35e30e36c1ccff7a9fc", "a69ee2ed945bcc6273a7d09edc59ef92", "0c92b79b4097640d4ead73efaa5bdd23")
+   
   end
   
   def first_input_response 
