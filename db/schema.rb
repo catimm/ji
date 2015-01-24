1342d2dde6b9a2caa5ba91fa04fdf7c61bf3f33a
Carl Timm <ctiv@hotmail.com>
2015-01-24 13:01:09 -0800

    Added FirstName to Users model and register page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112000043) do
+ActiveRecord::Schema.define(version: 20150124204942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -237,6 +237,7 @@ ActiveRecord::Schema.define(version: 20150112000043) do
     t.string   "unconfirmed_email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
