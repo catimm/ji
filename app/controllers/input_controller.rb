4bd49a23f483ef1815d4d0e8013f22e7cdaa78e4
Carl Timm <ctiv@hotmail.com>
2015-01-25 18:53:24 -0800

    Troubleshooting

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -17,7 +17,7 @@ class InputController < ApplicationController
     
     video = Video.new
     video.uuid = data_parsed["uuid"]
-    video.completed = data_parsed["formats"]["completed_at"]
+    video.completed = data_parsed["created_at"]
     
     if video.save
       input_video_url_path
