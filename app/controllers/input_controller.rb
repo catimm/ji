80bfe04a1ddc459ea85ad105d41e95dd3585d725
Carl Timm <ctiv@hotmail.com>
2015-01-26 14:13:36 -0800

    Testing hash array

--- app/controllers/input_controller.rb
+++ app/controllers/input_controller.rb
@@ -17,7 +17,7 @@ class InputController < ApplicationController
     
     video = Video.new
     video.uuid = data_parsed["uuid"]
-    video.completed = data_parsed['formats']["state"]
+    video.completed = data_parsed['formats'][0]['completed_at']
 
   end
   
