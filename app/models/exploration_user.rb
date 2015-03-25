3cf85508a24f567c80d81947b00c41dfad22d67b
Carl Timm <ctiv@hotmail.com>
2015-03-24 17:15:32 -0700

    Fixed logic on first project reminder email

--- app/models/exploration_user.rb
+++ app/models/exploration_user.rb
@@ -23,17 +23,20 @@ class ExplorationUser < ActiveRecord::Base
   belongs_to :exploration, counter_cache: true
   
   def self.project_reminder_email
-    started = ExplorationUser.where("started IS NOT ? AND completed IS ? AND reminders < ?", nil, nil, 3)
-    started.each do |d|
+    feedback_started = ExplorationUser.where("started IS NOT ? AND completed IS ? AND reminders < ?", nil, nil, 3)
+    feedback_started.each do |d|
       Rails.logger.debug("Started user info: #{d.inspect}")
       if d.reminders < 3
         invited = User.find(d.user_id)
         Rails.logger.debug("Invited info: #{invited.inspect}")
         description = Exploration.find(d.exploration_id)
         if d.reminders == 0
-          UserMailer.project_reminder_email(invited, description.short_description).deliver
-          d.update_attribute :last_reminder_sent, Time.now.utc
-          d.update_attribute :reminders, 1
+          sign_up = ((Time.now - d.started)/1.day).round
+          if sign_up > 3
+            UserMailer.project_reminder_email(invited, description.short_description).deliver
+            d.update_attribute :last_reminder_sent, Time.now.utc
+            d.update_attribute :reminders, 1
+          end
         else 
           last_sent = ((Time.now - d.last_reminder_sent)/1.day).round
           Rails.logger.debug("Days ago last sent: #{last_sent.inspect}")
